e25445b2b99e287998daf32ad10b69d7103fdd57
Vlad <10327965@stud.op.edu.ua>
2024-08-14 17:04:17 +0300

    startScreen without nav

---
+++ app/src/main/java/com/example/bunker/presentation/start_screen/StartScreen.kt
@@ -0,0 +1,101 @@
+package com.example.bunker.presentation.start_screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bunker.R
+
+@Preview(showBackground = true)
+@Composable
+fun StartScreen(modifier: Modifier = Modifier) {
+    Image(
+        painterResource(id = R.drawable.start_screen), contentDescription = "background",
+        modifier = Modifier.fillMaxSize(), contentScale = ContentScale.FillHeight
+    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+        Text(
+            text = "Бункєр",
+            fontSize = 48.sp,
+            fontStyle = FontStyle.Normal,
+            fontWeight = FontWeight.W900
+        )
+        Column(modifier = Modifier.padding(top = 64.dp)) {
+            Button(
+                modifier = Modifier
+                    .size(width = 222.dp, height = 66.dp)
+                    .padding(bottom = 4.dp),
+                onClick = { /*TODO*/ },
+                enabled = false,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black,
+                    contentColor = Color.Red,
+                    disabledContentColor = Color.Gray.copy(alpha = 0.5f),
+                    disabledContainerColor = Color.DarkGray.copy(alpha = 0.5f)
+                ),
+            ) {
+                Text(
+                    "Продовжити",
+                    fontSize = 24.sp,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.W400
+                )
+            }
+            Button(
+                modifier = Modifier
+                    .size(width = 222.dp, height = 66.dp)
+                    .padding(bottom = 4.dp),
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black,
+                    contentColor = Color.Red
+                )
+            ) {
+                Text(
+                    "Нова гра",
+                    fontSize = 24.sp,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.W400
+                )
+            }
+            Button(
+                modifier = Modifier
+                    .size(width = 222.dp, height = 66.dp)
+                    .padding(bottom = 4.dp),
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black,
+                    contentColor = Color.Red
+                )
+            ) {
+                Text(
+                    "Правила",
+                    fontSize = 24.sp,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.W400
+                )
+            }
+        }
+
+    }
+}
